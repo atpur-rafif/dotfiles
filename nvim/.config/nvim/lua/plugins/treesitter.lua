a9f59e50bd197d9c3fba0d212758b9935abf7219
atpur-rafif <muhammadatpurrafif@gmail.com>
2026-08-02 07:29:22 +0700

    feat: set treesitter auto highlight

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,41 +1,24 @@
 local function setup()
-	require("nvim-treesitter").setup({
-		highlight = {
-			enable = true,
-			disable = function(_, buf)
-				local max_filesize = 100 * 1024 -- 100 KB
-				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-				if ok and stats and stats.size > max_filesize then
-					return true
-				end
-			end,
-			additional_vim_regex_highlighting = false,
-		},
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "gnn",
-				node_incremental = "grn",
-				scope_incremental = "grc",
-				node_decremental = "grm",
-			},
-		},
-		indent = {
-			enable = false
-		},
-		autotag = {
-			enable = true
-		}
-	})
+	require("nvim-treesitter").setup({})
 
 	vim.api.nvim_create_autocmd('FileType', {
-		pattern = { 'python', 'c', 'typescript', 'javascript' },
-		callback = function() vim.treesitter.start() end,
+		pattern = { '*' },
+		callback = function(args)
+			local ft = vim.bo[args.buf].filetype
+			local lang = vim.treesitter.language.get_lang(ft)
+			if lang == nil then return end
+			local treesitter = require("nvim-treesitter")
+
+			if not vim.tbl_contains(treesitter.get_installed(), lang) then
+				return
+			end
+
+			vim.treesitter.language.add(lang)
+			vim.treesitter.start(args.buf, lang)
+		end,
 	})
 
-	require("nvim-autopairs").setup({
-		disable_filetype = { "TelescopePrompt" }
-	})
+	require("nvim-autopairs").setup({ disable_filetype = { "TelescopePrompt" } })
 
 	require("nvim-ts-autotag").setup({})
 
